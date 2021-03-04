8d41a73ec23fe05aefc9b05840842f1b87c047d0
jiyun park <jyun1937@naver.com>
2021-03-04 10:15:13 +0900

    port

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function clock() {
 var timer =new Date();
-console.log(timer);
+//console.log(timer);
 var mm = timer.getMonth() +1;
 var dd =timer.getDate();
 var day =timer.getDay();
@@ -14,29 +14,29 @@ if (dd < 10) dd= '0' + dd;
 if (h < 10) h= '0' + h;
 if (m < 10) m= '0' + m;
 if (s < 10) s= '0' + s;
-console.log(mm, dd, day, h , m, s); 
+//console.log(mm, dd, day, h , m, s); 
 var current = arrayMonth[mm-1]  +' ' +dd + ' '+arrayDay[day] +' '+h+':'+m+':'+s;
 // var current =h+':'+m+':'+s;
-console.log(current);
+//console.log(current);
 document.querySelector('.clock').innerHTML=current;
 }
 $(document).ready(function(){
     var _nav = $('.swiper-pagination-bullet');
-    _nav.attr('tabindex','-1');
-    _nav.eq(0).append('<a href="">'+'ABOUT'+'</a>');
-    _nav.eq(1).append('<a href="">'+'SKILL'+'</a>');
-    _nav.eq(2).append('<a href="">'+'PORTFOLIO'+'</a>');
-    _nav.eq(3).append('<a href="">'+'CONTACT'+'</a>');
+    _nav.eq(0).text('ABOUT');
+    _nav.eq(1).text('SKILL');
+    _nav.eq(2).text('PORFOLIO');
+    _nav.eq(3).text('CONTACT');
      clock();
     var time = setInterval(clock, 1000);
     var _ab2=$('#vd');
     var timer2 =new Date();
     var h2 = timer2.getHours();
-    console.log(h2);
-    if (h2>=5 && h2<6) _ab2.attr({src:'video/일출.mp4'});
-    if (h2>=6 && h2<18) _ab2.attr({src:'video/Clouds - 1154.mp4'});
-    if (h2>=18 && h2<=19) _ab2.attr({src:'video/일몰.mp4'});
-    if (h2> 19 && h2<5) _ab2.attr({src:'video/Full Moon - 2144.mp4'});
+    //console.log(h2);
+    if (h2>=5 && h2<6) _ab2.attr({src:'video/일출.mp4'}); //새벽 다섯시부터 여섯시전까지
+    if (h2>=6 && h2<18) _ab2.attr({src:'video/Clouds - 1154.mp4'});//새벽 여섯시부터 저녁 6시 전까지
+    if (h2>=18 && h2<=19) _ab2.attr({src:'video/일몰.mp4'});//저녁 6시부터 저녁 7시까지
+    if (h2>19 && h2<=23) _ab2.attr({src:'video/Full Moon - 2144.mp4'});//저녁7시부터 밤 11시까지
+    if ((h2>=0&&h2<5)) _ab2.attr({src:'video/Full Moon - 2144.mp4'});//밤12시부터새벽 다섯시전까지
  //버튼 클릭시 사진 변경이벤트
  $('.tb1').on('click',function(){
     _ab2.attr({src:'video/일출.mp4'});
@@ -59,5 +59,19 @@ $(document).ready(function(){
  });
  $('.tb5').on('click',function(){
  window.location.reload();
+});
+
+//ab1 마우스 움직이기
+$('.ab1').on('mousemove',function(e){
+   //마우스 진입했을때 black 없애기
+   //요소의  중앙 좌표값 구하기
+   var centerX = $(this).offset().left +$(this).width()/2;
+   var centerY = $(this).offset().top +$(this).height()/2;
+   console.log(centerX,centerY);
+//움직일 translate 값
+translateX = (e.pageX - centerX)/5;
+translateY = (e.pageY - centerY)/5;
+console.log(translateX,translateY);
+gsap.to('.ab1photo',{transform:'translate('+translateX+'px,'+translateY+'px)'});
 });
 });         
